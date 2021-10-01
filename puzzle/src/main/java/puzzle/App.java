8311815fa5bb82b9a143a353ed61b714205d8c20
sergiogmoraleda <sergio.garcia52@alu.uclm.es>
2021-10-01 20:24:40 +0200

    proyecto creado con maven

---
+++ puzzle/src/main/java/puzzle/App.java
@@ -0,0 +1,13 @@
+package puzzle;
+import com.google.gson.*;
+import java.util.Stack;
+import java.util.ArrayList;
+import java.util.Scanner;
+public class App 
+{
+    public static void main( String[] args )
+    {
+        
+        ArrayList<Stack> botellas = new ArrayList<Stack>();
+    }
+}
